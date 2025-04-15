90ddf914c9672f4d42a79c70b10f1236c0187769
ndwifi <66157075+ndwifi@users.noreply.github.com>
2025-04-15 18:32:01 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,9 +12,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+API_ID = config("21996245", default=None, cast=int)
+API_HASH = config("fc4a85a0f098e8434662193d12936f47", default=None)
+BOT_TOKEN = config("8106644785:AAGrBMWVuO-eYe6T3yQXkjvrcr5Y3U_L_Mg", default=None)
 SESSION = config("SESSION", default=None)
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None, cast=int)
